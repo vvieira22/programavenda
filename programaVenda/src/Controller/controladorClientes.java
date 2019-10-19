55db8325b0b13af5af46236d9d2e1aec71098a0a
vvieira22 <vitorgoncalvesvieira22@gmail.com>
2019-10-19 17:31:02 -0300

    Primeiro envio

---
+++ programaVenda/src/Controller/controladorClientes.java
@@ -0,0 +1,55 @@
+package Controller;
+
+import programavenda.Cliente;
+import java.util.ArrayList;
+
+public class controladorClientes {
+
+    private static controladorClientes instancia;
+    private ArrayList<Cliente> clientes;
+    private int quantidadeClientes;
+
+    private controladorClientes() {
+        clientes=new ArrayList<>();
+        quantidadeClientes=0;
+    }
+    
+    public static controladorClientes getInstance(){
+        if(instancia==null)
+            instancia=new controladorClientes();
+        return instancia;
+    }
+    
+    public boolean verificarCpfExistente(String cpf){
+        for (int i = 0; i < quantidadeClientes; i++) {
+            if(clientes.get(i).getCpf().equals(cpf))
+                return true;   
+        }
+        return false;
+    }
+    
+
+    public void excluirCliente(String nome){
+        for (int i = 0; i < quantidadeClientes; i++) {
+            if(clientes.get(i).getNome().equals(nome)){
+                clientes.remove(i);
+                break;
+                //dizer que removeu
+            }   
+        } 
+    }
+    
+    public void inserirCliente(String nome, String cpf, String telefoneum, String telefonedois, String email, String rua, int numero, String bairro, String complemento){
+        if(verificarCpfExistente(cpf)==false)
+            clientes.add(new Cliente(nome, cpf, telefoneum, telefonedois, email, rua, numero, bairro, complemento));            
+        }
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ programaVenda/src/programavenda/Cliente.java
@@ -0,0 +1,98 @@
+package programavenda;
+
+public class Cliente {
+       
+    private String nome;
+    private String cpf;
+    private String telefoneum;
+    private String telefonedois;
+    private String email;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String complemento;
+
+    public Cliente(String nome, String cpf, String telefoneum, String telefonedois, String email, String rua, int numero, String bairro, String complemento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefoneum = telefoneum;
+        this.telefonedois = telefonedois;
+        this.email = email;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.complemento = complemento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTelefoneum(String telefoneum) {
+        this.telefoneum = telefoneum;
+    }
+
+    public void setTelefonedois(String telefonedois) {
+        this.telefonedois = telefonedois;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefoneum() {
+        return telefoneum;
+    }
+
+    public String getTelefonedois() {
+        return telefonedois;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }  
+}
